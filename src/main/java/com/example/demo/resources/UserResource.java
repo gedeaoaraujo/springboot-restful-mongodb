6432a543212978ea664d422182ba77442d99096c
Gedeao <gedeao.araujo@outlook.com.br>
2020-01-20 20:32:01 -0300

    Instantiation of the database

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.example.demo.resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.domain.User;
