99f160e5eb292b71b0c6c37a8c7c19ce45cdb167
Gedeao <gedeao.araujo@outlook.com.br>
2020-01-20 20:50:04 -0300

    Using pattern DTO to return an users list

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService service;
 
    @GetMapping
-   public ResponseEntity<List<User>> findAll() {
+   public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
    }
     
 }
